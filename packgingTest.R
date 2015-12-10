90bcb13a87879fdb861f231d1cf4c76de1b4e650
Boying Gong <jorothy_gong@berkeley.edu>
2015-12-09 19:32:27 -0800

    add density plot, qq plot, Kolmogorov-Smirnov Tests, Cram´er-Von Mises (CvM) Test, Anderson-Darling (AD) Test under standard normal case

--- packgingTest.R
+++ packgingTest.R
@@ -1,3 +1,54 @@
-add <- function(x, y) {
-  x + y
-}
+sv <- simulate_things(dnorm, c(-Inf,Inf), n = 10000)
+print(length(sv))
+histogram(sv, col='blue', prob=TRUE, bw=.5)
+
+
+###################     test     ###################
+
+## density plot
+
+# add plots comparing the simlated density and the actual density
+# for our adaptive rejection sampling function and built-in R function
+
+par(mfrow = c(1, 2))
+
+plot(density(sv), main = "ARS")
+lines(a <- seq(-4.5, 4.5, by = 0.05), dnorm(a), col='blue')
+
+plot(density(rnorm(10000)), main = "built-in fuuction")
+lines(a <- seq(-4.5, 4.5, by = 0.05), dnorm(a), col='blue')
+
+dev.off()
+
+## qq plot
+
+qqplot(qnorm(ppoints(500)), sv,
+main = expression("Q-Q plot: AQS for std. Normal"))
+
+## Kolmogorov-Smirnov Tests
+
+ks_res <- ks.test(sv, pnorm)
+ks_res_bt <- ks.test(rnorm(10000), pnorm)
+
+## Cram´er-Von Mises (CvM) Test
+
+install.packages("goftest")
+library(goftest)
+cvm_res <- cvm.test(sv, "pnorm", mean=0, sd=1)
+cvm_res_bt <- cvm.test(rnorm(10000), "pnorm", mean=0, sd=1)
+
+## Anderson-Darling (AD) Test
+
+library(goftest)
+ad_res <- ad.test(sv, "pnorm", mean=0, sd=1)
+ad_res_bt <- cvm.test(rnorm(10000), "pnorm", mean=0, sd=1)
+
+## summary table
+
+summary <- data.frame(c(ks_res$p.value, cvm_res$p.value, ad_res$p.value),
+c(ks_res$p.value > 0.05, cvm_res$p.value > 0.05, ad_res$p.value > 0.05),
+c(ks_res_bt$p.value, cvm_res_bt$p.value, ad_res_bt$p.value),
+row.names = c("Kolmogorov-Smirnov","Cram´er-Von Mises","Anderson-Darling"))
+colnames(summary) <- c("ARS p-value","Significant", "Built-in p-value")
+print(summary)
+
