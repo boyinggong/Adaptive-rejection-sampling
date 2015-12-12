3c07a3c441db78e2c36b3ae988b5553deab36c07
ye-zhi <yezhi@berkeley.edu>
2015-12-12 12:42:26 -0800

    test

--- ars/R/dh.R
+++ ars/R/dh.R
@@ -1,8 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
+
 # You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
@@ -13,6 +9,8 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+
+# calculate the derivative of logarithm of functions
 dh <- function(x,g) {
     x <- as.numeric(x)
     return(mosaic::D(log(g(x)) ~ x)(x))
--- ars/tests/testthat/test-get_zj.R
+++ ars/tests/testthat/test-get_zj.R
@@ -1,1 +1,14 @@
 context("Intersection points")
+
+test_that("special cases are correct", {
+    print("special cases are correct")
+    expect_equal(get_zj(c(1,2,3), c(4,8,12), c(1, 0,-1)), c(5,7))
+})
+
+test_that("throw errors for inputs error", {
+    print("throw errors when vector length is not consistent")
+    expect_error(get_zj(c(1,2,3), c(4,8), c(1, 0, 1), c(-1, 0,  1)), "input length inconsistent")
+    expect_error(get_zj(c(1,2,3), c(4,8,12), c(1, 0)), "input length inconsistent")
+    print("throw errors when number of points is less than 2")
+    expect_error(get_zj(c(1), c(4), c(1)), "number of points should be greater than 2")
+})
